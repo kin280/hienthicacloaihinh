824372b0b522e8c50dde197caf2dac4aececb96a
Tho Kien <74567462+kin280@users.noreply.github.com>
2023-12-11 20:07:24 +0700

    Add project files.

---
+++ hienthicacloaihinh/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int choice = -1;
+        while (choice != 0)
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1. In hình chữ nhật");
+            Console.WriteLine("2. In hình tam giác vuông");
+            Console.WriteLine("3. In tam giác cân");
+            Console.WriteLine("0. Thoat");
+            Console.WriteLine("Enter your choice: ");
+            choice = Int32.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        for (int j = 0; j < 7; j++)
+                        {
+                            Console.Write("* ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 2:
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            Console.Write("* ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 3:
+                    for (int i = 5; i >= 1; i--)
+                    {
+                        for (int j = 1; j <= i; j++)
+                        {
+                            Console.Write("* ");
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("No choice!");
+                    break;
+            }
+
+        }
+    }
+}
